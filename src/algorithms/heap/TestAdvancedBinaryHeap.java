47fc07fe5367579eb3e8e863268c09ee8c9892fd
moqiguzhu <moqiguzhu@115.154.123.86>
2015-07-21 20:11:35 +0800

    - Correct errors for TestAdvancedBinaryHeap.java

--- src/algorithms/heap/TestAdvancedBinaryHeap.java
+++ src/algorithms/heap/TestAdvancedBinaryHeap.java
@@ -1,10 +1,5 @@
 package algorithms.heap;
 
 public class TestAdvancedBinaryHeap {
-	public static void main(String[] args) {
-		A a1 = new A();
-		B b1 = new B();
-		System.out.println(b1 instanceof B);
-		System.out.println(b1 instanceof A);
-	}
+
 }
