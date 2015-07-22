0b2c9117da86491ac2c9ba53e9032182e6afc228
moqiguzhu <moqiguzhu@115.154.123.86>
2015-07-23 00:14:09 +0800

    - Add some unit tests

--- src/algorithms/heap/AdvancedBinaryHeap.java
+++ src/algorithms/heap/AdvancedBinaryHeap.java
@@ -12,14 +12,16 @@ import java.util.Map;
  *
  * @param <E>
  *            E是堆中存储的真实数据的类型
- *            Java给的API中，优先队列的实现采用PriorityQueue，但是这个结构在删除队列中某个元素的时候是线性时间的。
- *            于是我实现了这个类，这个heap类删除元素的时间是O(log(n))的，并且增加了动态修改元素key信息的功能。
- *            堆中元素的信息是根据key信息来确定的。 这个类可以用来实现快速的Dijkstra算法和Prim MST算法
  */
+
+// Java给的API中，优先队列的实现采用PriorityQueue，但是这个结构不支持在堆中快速查找元素
+// 于是我实现了这个类，这个heap类删除元素的时间是O(log(n))的，并且增加了动态修改元素key信息的功能。
+// 堆中元素的信息是根据key信息来确定的。 这个类可以用来实现快速的Dijkstra算法和Prim MST算法
+
 // 添加对key的泛型支持，类的正确定义应该是AdvancedBinaryHeap<E, K>
 // 处理异常，强制类型转换报warning的地方需要修改
 // 因为采用Map存储数据，这就要求堆中不能有两个完全一样的数据(缺陷？)
-// 单元测试
+// 泛型单元测试
 public class AdvancedBinaryHeap<E, K> {
 	/* 保存节点在heap中的位置信息 */
 	private Map<E, Integer> element_index;
@@ -27,8 +29,8 @@ public class AdvancedBinaryHeap<E, K> {
 	private List<E> data;
 	/* 保存节点的key信息，先后顺序是使用key来确定的 */
 	private Map<E, K> element_key;
-	/* using MAX to initiate key values of nodes */
-	private double MAX = Double.MAX_VALUE;
+	/* 用于比较key */
+	Comparator<? super K> comparator = null;
 
 	public void init() {
 		element_index = new HashMap<E, Integer>();
@@ -51,6 +53,9 @@ public class AdvancedBinaryHeap<E, K> {
 			element_index.put(element, i + 1);
 			element_key.put(element, k.get(i));
 		}
+		for (int i = data.size() / 2; i > 0; i--) {
+			bubbleDown(data.get(i));
+		}
 	}
 
 	public AdvancedBinaryHeap(int initialCapacity) {
@@ -60,13 +65,29 @@ public class AdvancedBinaryHeap<E, K> {
 		element_key = new HashMap<E, K>();
 	}
 
-	// !!! to-do
-	public AdvancedBinaryHeap(int initialCapacity, Comparator<? super Double> comparator) {
-
+	//
+	public AdvancedBinaryHeap(int initialCapacity, Comparator<? super K> comparator) {
+		element_index = new HashMap<E, Integer>();
+		data = new ArrayList<E>(initialCapacity + 1);
+		data.add(null); // sentinel， 数组的第一个空间不使用，方便left和right函数的编写
+		element_key = new HashMap<E, K>();
+		this.comparator = comparator;
 	}
 
-	public Map<E, K> getKeys() {
+	public Map<E, K> getElement_Keys() {
 		return element_key;
+	}
+
+	protected List<E> getData() {
+		return data;
+	}
+
+	protected Map<E, Integer> getElement_index() {
+		return element_index;
+	}
+
+	protected Comparator<? super K> getComparator() {
+		return comparator;
 	}
 
 	// return the index of parent
@@ -90,23 +111,26 @@ public class AdvancedBinaryHeap<E, K> {
 
 	public int compareKey(K k1, K k2) {
 		// Cannot perform instanceof check against parameterized type
-		// Comparator<? super K>.
-		// Use the form Comparator<?> instead since further generic type
-		// information will
-		// be erased at runtime
+		// Comparator<? super K>.Use the form Comparator<?> instead since
+		// further generic type information will be erased at runtime
 		int result = 0;
-		try {
-			if (k1 instanceof Comparator<?> && k2 instanceof Comparator<?>) {
-				Comparator<? super K> comparator = (Comparator<? super K>) k1;
-				result = comparator.compare(k1, k1);
-			} else if (k1 instanceof Comparable<?> && k2 instanceof Comparable<?>) {
-				result = ((Comparable<K>) k1).compareTo(k2);
-			} else {
-				//
+		if (comparator != null) {
+			return comparator.compare(k1, k2);
+		} else {
+			try {
+				if (k1 instanceof Comparator<?> && k2 instanceof Comparator<?>) {
+					Comparator<? super K> localComparator = (Comparator<? super K>) k1;
+					result = localComparator.compare(k1, k1);
+				} else if (k1 instanceof Comparable<?> && k2 instanceof Comparable<?>) {
+					result = ((Comparable<K>) k1).compareTo(k2);
+				} else {
+					throw new ClassCastException();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
+
 		return result;
 	}
 
@@ -162,16 +186,18 @@ public class AdvancedBinaryHeap<E, K> {
 		K oldKey = element_key.get(element);
 		element_key.put(element, newKey);
 		// 逐级下降
-		if (compareKey(newKey, oldKey) > 0)
+		if (compareKey(newKey, oldKey) > 0) {
 			bubbleDown(element);
+		}
 		// 逐级上升
-		if (compareKey(newKey, oldKey) < 0)
+		if (compareKey(newKey, oldKey) < 0) {
 			bubbleUp(element);
+		}
 	}
 
-	// !!! to-do
-	public Comparator<? super Double> comparator() {
-		return null;
+	// 有可能返回null
+	public Comparator<? super K> comparator() {
+		return this.comparator;
 	}
 
 	public boolean contains(Object object) {
@@ -183,7 +209,9 @@ public class AdvancedBinaryHeap<E, K> {
 	}
 
 	public boolean offer(E element, K key) {
+		data.add(element);
 		element_key.put(element, key);
+		element_index.put(element, size());
 		bubbleUp(element);
 		return true; // 处理异常
 	}
@@ -226,12 +254,19 @@ public class AdvancedBinaryHeap<E, K> {
 		return elementAndkey;
 	}
 
+	// O(n)的时间复杂度
+	// 并且存在错误，因为如果两个element的key是一样的话，它们在数组中的位置实际上是任意的
+	@Deprecated
 	public boolean remove(Object object) {
-		//
 		E element = (E) object;
 		if (!contains(object)) {
 			return false;
 		} else {
+			for (E e : element_index.keySet()) {
+				if (compareKey(element_key.get(element), element_key.get(e)) < 0) {
+					element_index.put(e, element_index.get(e) - 1);
+				}
+			}
 			int index = element_index.get(element);
 			data.remove(index);
 			element_key.remove(element);
@@ -239,5 +274,51 @@ public class AdvancedBinaryHeap<E, K> {
 
 			return true;
 		}
+	}
+
+	public static void main(String[] args) {
+		AdvancedBinaryHeap<Integer, Double> heap = new AdvancedBinaryHeap<Integer, Double>();
+
+		// test offer function
+		heap.offer(1, 2.0);
+		System.out.println(heap.getData()); // should be {null, 1}
+		heap.offer(2, 3.0);
+		System.out.println(heap.getData()); // should be {null, 1, 2}
+
+		// test setKey function
+		heap.setKey(1, 100.0);
+		System.out.println(heap.getData()); // should be {null, 2, 1}
+		heap.setKey(2, 1000.0);
+		System.out.println(heap.getData()); // should be {null, 1, 2}
+		System.out.println(heap.getElement_Keys()); // shoule be {1=100.0,
+													// 2=1000.0}
+
+		// test peek function
+		System.out.println(heap.peek()); // should be {1, 100.0}
+		System.out.println(heap.getData()); // should be {null, 1, 2}
+
+		// test poll function
+		System.out.println(heap.poll()); // should be {1, 100.0}
+		System.out.println(heap.getData()); // should be {null,2}
+
+		// test contains function
+		heap.offer(3, 4.0);
+		System.out.println(heap.contains(3)); // should be true
+		System.out.println(heap.contains(2)); // should be true
+		System.out.println(heap.contains(1)); // should be false
+
+		// test compareKey function
+		int temp = heap.compareKey(heap.getElement_Keys().get(2), heap.getElement_Keys().get(3));
+		System.out.println(temp > 0); // should be true
+		heap.offer(4, 10000.0);
+		temp = heap.compareKey(heap.getElement_Keys().get(2), heap.getElement_Keys().get(4));
+		System.out.println(temp > 0); // should be false
+
+		// test remove function
+		heap.remove(2);
+		System.out.println(heap.getData()); // should be {null, 3, 4}
+		System.out.println(heap.getElement_index());// should be {3=1, 4=2}
+		
+		//
 	}
 }
--- src/algorithms/heap/TestAdvancedBinaryHeap.java
+++ src/algorithms/heap/TestAdvancedBinaryHeap.java
@@ -1,5 +1,121 @@
 package algorithms.heap;
 
-public class TestAdvancedBinaryHeap {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.junit.Test;
+
+class Element {
+	private int id;
+	public Element(int id) {
+		// TODO Auto-generated constructor stub
+		this.id = id;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		Element element = (Element)o;
+		return this.id == element.id;
+	}
+	
+	@Override
+	public String toString() {
+		return "" + id;
+	}
+}
+
+class Key implements Comparator<Key> {
+	private int key;
+	
+	public Key(int key) {
+		// TODO Auto-generated constructor stub
+		this.key = key;
+	}
+	
+	@Override
+	public int compare(Key o1, Key o2) {
+		// TODO Auto-generated method stub
+		return o1.key - o2.key;
+	}
+	
+	@Override
+	public boolean equals(Object o1) {
+		Key k1 = (Key)o1;
+		return k1.key == this.key;
+	}
+}
+
+//单元测试也要写泛型？
+public class TestAdvancedBinaryHeap {
+	private List<Integer> expectedData;
+	private Map<Integer, Integer> expectedElement_index;
+	private Map<Integer, Double> expectedElement_key;
+	
+	private List<Element> expectedData1;
+	private Map<Element, Integer> expectedElement_index1;
+	private Map<Element, Key> expectedElement_key1;
+	
+	@Test
+	public void testConstrutor() {
+		AdvancedBinaryHeap<Integer, Double> heap = new AdvancedBinaryHeap<Integer, Double>();
+		org.junit.Assert.assertNotNull("should not be null", heap.getData());
+		org.junit.Assert.assertNull("should be null", heap.getComparator());
+		org.junit.Assert.assertNotNull("should not be null", heap.getElement_index());
+		org.junit.Assert.assertNotNull("should not be null", heap.getElement_Keys());
+		org.junit.Assert.assertEquals("failure - int should be equal", 0, heap.size());	
+		
+		AdvancedBinaryHeap<Element, Key> heap1 = new AdvancedBinaryHeap<Element, Key>();
+		org.junit.Assert.assertNotNull("should not be null", heap.getData());
+		org.junit.Assert.assertNull("should be null", heap.getComparator());
+		org.junit.Assert.assertNotNull("should not be null", heap.getElement_index());
+		org.junit.Assert.assertNotNull("should not be null", heap.getElement_Keys());
+		org.junit.Assert.assertEquals("failure - int should be equal", 0, heap.size());
+	}
+	
+	@Test
+	public void testOffer() {
+		AdvancedBinaryHeap<Integer, Double> heap = new AdvancedBinaryHeap<Integer, Double>();
+		heap.offer(1, 2.0);
+		heap.offer(2, 3.0);
+		expectedData = new ArrayList<Integer>(Arrays.asList(null,1,2));
+		org.junit.Assert.assertEquals("should be equal", heap.getData(), expectedData);
+		
+		expectedElement_index = new HashMap<Integer, Integer>();
+		expectedElement_index.put(1, 1);
+		expectedElement_index.put(2, 2);
+		org.junit.Assert.assertEquals("should be equal",heap.getElement_index(), expectedElement_index);
+		
+		AdvancedBinaryHeap<Element, Key> heap1 = new AdvancedBinaryHeap<Element, Key>();
+		heap1.offer(new Element(1), new Key(2));
+		heap1.offer(new Element(2), new Key(3));
+		expectedData1 = new ArrayList<Element>(Arrays.asList(null, new Element(1), new Element(2)));
+		org.junit.Assert.assertEquals("should be equal",heap1.getData(), expectedData1);
+		
+		expectedElement_index1 = new HashMap<Element, Integer>();
+		expectedElement_index1.put(new Element(1), 1);
+		expectedElement_index1.put(new Element(2), 2);
+		System.out.println(expectedElement_index1);
+		//如何比较两个Map相等
+		
+	}
+	
+	@Test
+	public void testSetKey() {
+		AdvancedBinaryHeap<Integer, Double> heap = new AdvancedBinaryHeap<>();
+		heap.offer(1, 2.0);
+		heap.offer(2, 3.0);
+		heap.setKey(1, 100.0);
+		
+		expectedElement_key = new HashMap<Integer, Double>();
+		expectedElement_key.put(1, 100.0);
+		expectedElement_key.put(2, 3.0);
+		org.junit.Assert.assertEquals("should be equal",heap.getElement_Keys(), expectedElement_key);
+		
+		expectedData = new ArrayList<Integer>(Arrays.asList(null, 2, 1));
+		org.junit.Assert.assertEquals("should be equal", heap.getData(), expectedData);
+	}
 }
