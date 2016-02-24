8a63cc79e505357df46926939776eac540f159d8
moqiguzhu <moqiguzhu@163.com>
2016-02-24 08:58:28 +0800

    modify BinarySearch class

--- src/algorithms/binarysearch/BinarySearch.java
+++ src/algorithms/binarysearch/BinarySearch.java
@@ -2,42 +2,55 @@ package algorithms.binarysearch;
 
 import java.util.Arrays;
 
-// this is a class which contains all kinds of functions related with binary search
+/**
+ * this is a class which contains all kinds of functions related with binary search
+ * 
+ * @author moqiguzhu
+ * @date 2016-02-24
+ * @version 1.0
+ */
 public class BinarySearch {
   public boolean binarySearch(int[] sortedArr, int target) {
     return Arrays.binarySearch(sortedArr, target) >= 0;
   }
-  
-  // left: inclusive
-  // right: exclusive
+
   public boolean binarySearch(int[] sortedArr, int left, int right, int target) {
     return Arrays.binarySearch(sortedArr, left, right, target) >= 0;
   }
-  
-  // return index that most less or equal than target
-  // x >= target least
-  // left: inclusive
-  // right: exclusive
-  public int IndexMostLessOrEqual(int[] sortedArr, int left, int right, int target) {
+
+  /**
+   * 
+   * @param sortedArr sorted array
+   * @param left inclusive
+   * @param right exclusive
+   * @param target target element
+   * @return return index that sortedArr[index] least great or equal than target. return
+   *         sortedArr.length means that sortedArr[sortedArr.length-1] < target
+   */
+  public int IndexLeastGreatOrEqual(int[] sortedArr, int left, int right, int target) {
     assert(right > left);
-    while(right > left) {
+    while (right > left) {
       int mid = ((right - left) >> 1) + left;
-      if(target <= sortedArr[mid]) {
+      if (target <= sortedArr[mid]) {
         right = mid;
       } else {
         left = mid + 1;
       }
     }
-    // right is the index that sortedArr[right] < target most
-    
+
     return left;
   }
-  
-  // return index that least great or equal than target
-  // x <= target most
-  // left: inclusive
-  // right: exclusive
-  public int IndexLeastGreatOrEqual(int[] sortedArr, int left, int right, int target) {
+
+  /**
+   * 
+   * @param sortedArr sortedArray
+   * @param left inclusive
+   * @param right exclusive
+   * @param target target element
+   * @return return index that sortedArr[index] most less or equal than target. return -1 means
+   *         sortedArr[0] > target
+   */
+  public int IndexMostLessOrEqual(int[] sortedArr, int left, int right, int target) {
     assert(right > left);
     while (left < right) {
       int mid = ((right - left) >> 1) + left;
@@ -49,11 +62,11 @@ public class BinarySearch {
     }
     return --left;
   }
-  
+
   public static void main(String[] args) {
     BinarySearch bs = new BinarySearch();
-    
-    int[] nums1 = new int[]{1,3,5,5,5,7,9};
-    System.out.println(bs.IndexMostLessOrEqual(nums1, 0, 7, 5));
+
+    int[] nums1 = new int[] {1, 3, 5, 5, 5, 7, 9};
+    System.out.println(bs.IndexLeastGreatOrEqual(nums1, 0, 7, 10));
   }
 }
